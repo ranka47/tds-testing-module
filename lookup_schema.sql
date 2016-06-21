0aef42584c2540ed0f8601895ef1fc8f358bf24a
Sumeet Ranka <sumeet.ranka47@gmail.com>
2016-06-21 19:27:38 +0530

    Added unique columns in test schema

--- lookup_schema.sql
+++ lookup_schema.sql
@@ -5,5 +5,7 @@ create table runsheet_idx(id bigint, next_id bigint, cache bigint, increment big
 create table date_runsheet_idx(createdatetime datetime, runsheetid bigint(20), primary key(createdatetime, runsheetid));
 create table vindex_one(vindex int not null, sk int not null, PRIMARY KEY(vindex, sk));
 create table vindex_two(vindex int not null, sk int not null, PRIMARY KEY(vindex, sk));
+create table vindex_one_unique(vindex_unique int not null, sk int not null, PRIMARY KEY(vindex_unique));
+create table vindex_two_unique(vindex_unique int not null, sk int not null, PRIMARY KEY(vindex_unique));
 create table test_one_idx(id bigint,next_id bigint,cache bigint,increment bigint, primary key(id)) comment 'vitess_sequence';
 create table test_two_idx(id bigint,next_id bigint,cache bigint,increment bigint, primary key(id)) comment 'vitess_sequence';
